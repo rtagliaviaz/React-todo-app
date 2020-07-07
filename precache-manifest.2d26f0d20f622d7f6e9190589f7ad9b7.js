ef50fb1ade694b2783ff9abad2c91f7ee518ba3a
robert <robert5638@gmail.com>
2020-07-07 18:00:36 -0400

    Updates

---
+++ precache-manifest.2d26f0d20f622d7f6e9190589f7ad9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb83ff800146f71a1de94986a804fdb",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d6f5953bc8875b525952",
+    "url": "/react-todo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4d69fac1252f4b4b3b9f",
+    "url": "/react-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6f5953bc8875b525952",
+    "url": "/react-todo/static/js/2.f37fb70b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.f37fb70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d69fac1252f4b4b3b9f",
+    "url": "/react-todo/static/js/main.b26682e2.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
